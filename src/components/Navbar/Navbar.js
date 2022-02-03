2ab7d53d126af211e92b29cc6aeee12aee617991
mrwhitehat07 <bishalmagr70@gmail.com>
2022-02-03 22:18:34 +0545

    design updated 1.0

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,24 +2,100 @@ import { Link } from "react-router-dom";
 
 export default function Example() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+            height="15"
+            alt="MDB Logo"
+            loading="lazy"
+          />
+        </a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Dashboard</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Link</a>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Projects</a>
           </li>
         </ul>
-        <form className="d-flex">
-          <Link className="btn btn-outline-success" to="/login">Login</Link>
-        </form>
+      </div>
+      <div class="d-flex align-items-center">
+        <div class="dropdown">
+          <a
+            class="text-reset me-3 dropdown-toggle hidden-arrow"
+            href="#"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-mdb-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i class="fas fa-bell"></i>
+            <span class="badge rounded-pill badge-notification bg-danger">1</span>
+          </a>
+          <ul
+            class="dropdown-menu dropdown-menu-end"
+            aria-labelledby="navbarDropdownMenuLink"
+          >
+            <li>
+              <a class="dropdown-item" href="/">Some news</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/">Another news</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/">Something else here</a>
+            </li>
+          </ul>
+        </div>
+        <div class="dropdown">
+          <a
+            class="dropdown-toggle d-flex align-items-center hidden-arrow"
+            href="#"
+            id="navbarDropdownMenuAvatar"
+            role="button"
+            data-mdb-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+              class="rounded-circle"
+              height="25"
+              alt="Black and White Portrait of a Man"
+              loading="lazy"
+            />
+          </a>
+          <ul
+            class="dropdown-menu dropdown-menu-end"
+            aria-labelledby="navbarDropdownMenuAvatar"
+          >
+            <li>
+              <a class="dropdown-item" href="#">My profile</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Settings</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { getLocations } from "../../data/locations";
 
 export default function Home () {
@@ -10,30 +11,27 @@ export default function Home () {
         country: '',
     }]);
 
-    useEffect(() => {
-       
-    async function getLocs(){
-        const locs = await getLocations();
-        setLocations(locs);
-    }
-
-    getLocs();
-    console.log(locations);
+    useEffect(() => {   
+        async function getLocs(){
+            const locs = await getLocations();
+            setLocations(locs);
+        }
+        getLocs();
     },[]);
 
     return (
-        <>
-            <h1>Locations Popular</h1>
-            <div>
-                
+        <div className="container">
+            <h3>Popular Locations</h3>
+            <div>  
                 {
-                    locations.map((e) => (<div className="card px-2 py-2 bg-primary flex flex-column" onClick={() => console.log(e.slug)}>
-                        <p>{e.city}</p>
-                        <p>{e.country}</p>
-                    </div>))
-                }
-                
+                    locations.map((e) => (
+                        <Link className="card px-2 py-2 bg-primary flex flex-column" to={`/location/${e.slug}`}>
+                            <p className="text-white text-lg">{e.city}</p>
+                            <p className="text-white">{e.country}</p>
+                        </Link>
+                    ))
+                }   
             </div>
-        </>
+        </div>
     )
 }
